3034c3f371e9ea8ad54bffd45248c0f6e20760ff
neil patel <neil@myindianweddingapp.com>
2017-09-17 15:26:45 +0100

    Updated form submission

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -2,16 +2,40 @@ var Appointments = React.createClass({
   getInitialState: function() {
     return {
       appointments: this.props.appointments,
-      input_title: 'Team Standup Meeting',
-      input_appt_time: 'Tomorrow at 9am'
+      title: 'Team Standup Meeting',
+      appt_time: 'Tomorrow at 9am'
     }
+  },
+
+  handleUserInput: function(obj) {
+    this.setState(obj);
+  },
+
+  handleFormSubmit: function() {
+    var appointment = {title: this.state.title, appt_time: this.state.appt_time};
+    $.post('/appointments',
+            {appointment: appointment})
+          .done(function(data) {
+            this.addNewAppointment(data);
+          }.bind(this));
+  },
+
+  addNewAppointment: function(appointment) {
+    var appointments = React.addons.update(this.state.appointments, { $push: [appointment]});
+    this.setState({ 
+      appointments: appointments.sort(function(a,b){
+        return new Date(a.appt_time) - new Date(b.appt_time);
+      })
+    });
   },
 
   render: function() {
     return (
       <div>
-        <AppointmentForm input_title={this.state.input_title}
-        input_appt_time={this.state.input_appt_time} />
+        <AppointmentForm title={this.state.title}
+          input_appt_time={this.state.appt_time}
+          onUserInput={this.handleUserInput}
+          onFormSubmit={this.handleFormSubmit} />
         <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
